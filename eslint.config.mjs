e58eb36b90af5c0b6b04cca7beb1a5531cdc62d6
sarotnem <kkyriakos94@gmail.com>
2025-03-05 18:04:04 +0100

    chore: more eslint config and fixes

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,7 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
+import { FlatCompat } from '@eslint/eslintrc';
+import stylistic from '@stylistic/eslint-plugin';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -10,7 +11,14 @@ const compat = new FlatCompat({
 });
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+  ...compat.extends('next/core-web-vitals', 'next/typescript'),
+  stylistic.configs.customize({
+    // the following options are the default values
+    indent: 2,
+    quotes: 'single',
+    semi: true,
+    jsx: true,
+  }),
 ];
 
 export default eslintConfig;
