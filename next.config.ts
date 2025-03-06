1b40e08c534b55a6c99aa5666eb3f2fe03c4596a
sarotnem <kkyriakos94@gmail.com>
2025-03-06 10:56:44 +0100

    feat: Update tailwind theme and font

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,17 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'coverartarchive.org',
+        port: '',
+        pathname: '/release-group/**',
+        search: '',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
